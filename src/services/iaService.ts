67bc067020b426c00851af2637f59134235bfe20
Davi Alves <davialves.xy@gmail.com>
2025-04-27 04:17:38 -0300

    Ia funcionando

--- src/services/iaService.ts
+++ src/services/iaService.ts
@@ -1,9 +1,23 @@
 import "dotenv/config";
+import axios from "axios";
 
 class IA {
-    private text: string;
-    constructor(text: string = "Hello, World!") {
-        this.text = text;
+    private prompt: string;
+    private model: string;
+    private url: string;
+    constructor(prompt: string = "Hello, World!", model: string) {
+        this.prompt = prompt;
+        this.model = model;
+        this.url = "http://localhost:11434/api/generate";
+    }
+
+    async cURLPostIAPIGen() {
+        let response = await axios.post(this.url, {
+            "model": this.model,
+            "prompt": this.prompt,
+            "stream": false
+        })
+        return response.data.response
     }
 }
 
--- src/sketch.ts
+++ src/sketch.ts
@@ -1,15 +1,13 @@
 // ... Voice
-// import { play } from "elevenlabs";
-// import Voice from "./services/voiceService";
-// import { Readable } from "stream";
+import { play } from "elevenlabs";
+import Voice from "./services/voiceService";
+import { Readable } from "stream";
 // ... IA
 import IA from "./services/iaService"
 
+const responseIA = new IA("Olá, meu nome é Davi!", "phi4").cURLPostIAPIGen()
+console.log(await responseIA)
 
+const audio: Readable = await new Voice(await responseIA, "pqHfZKP75CvOlQylNhV4", "eleven_multilingual_v2").genAudio();
 
-
-
-
-// const audio: Readable = await new Voice("Vamos mudar um pouco o audio!", "pqHfZKP75CvOlQylNhV4", "eleven_multilingual_v2").genAudio();
-
-// await play(audio);
+await play(audio);
